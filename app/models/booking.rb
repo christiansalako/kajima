542291162fb8ee183c426e11dc395d75b3776d6d
christian Salako <47796176+christiansalako@users.noreply.github.com>
2023-03-02 05:03:03 +0000

    Configure api route, added state machine to Boooking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,10 +1,82 @@
 # frozen_string_literal: true
 
 class Booking < ApplicationRecord
+  include AASM
+
   belongs_to :user
   belongs_to :host, optional: true
   belongs_to :confirmed_by, class_name: 'User', optional: true
 
   validates :start_time, presence: true
   validates :end_time, presence: true
+
+  aasm do
+    state :provisional
+    state :confirmed
+    state :cancelled
+    state :expired
+
+    event :confirmed do
+      transitions from: [:provisional], to: :confirmed
+    end
+
+    event :cancel do
+      transitions from: [:confirmed], to: :cancelled
+    end
+
+    event :expiry do
+      transitions from: [:provisional], to: :expired
+    end
+  end
+
+  def confirmed_state_booking(booking)
+    return false if booking.state == 'confirmed' && booking.aasm_state == 'provisional'
+    true
+  end
+
+  def approve_user(booking, user)
+    return true if user.approved && booking.user_id == user.id
+    false
+  end
+
+  def time_approve(booking)
+    booking.start_time > Time.now
+  end
+
+  def cancelled_booking?(booking)
+    booking.state == 'cancelled' ? false : true
+  end
+
+  def expired_booking?(booking)
+    true if booking.end_time > Time.now
+  end
+
+  def time_stamp_approve(booking)
+    booking.confirmed_at = Time.now
+  end
+
+  def booked_by_user(booking, user)
+    booking.confirmed_by_id = user.id
+  end
+
+  def confirmation_type(params, user, booking)
+    confirmation_type = params["confirmation_type"]
+
+    case confirmation_type
+    when "email"
+      BookingMailer.with(user: user, booking: booking).booking_confirm.deliver_now
+    when "sms"
+      SMSSender.new(user.mobile, 'you have a message').deliver
+    else
+      BookingMailer.with(user: user, booking: booking).booking_confirm.deliver_now
+    end
+  end
+
+  def host_and_user_message(params, user, booking)
+    # send email to host and user if below present
+    if params["send_confirmation_msg"].present?
+      SMSSender.new(user.mobile, 'you have a message').deliver
+      BookingMailer.with(user: user, booking: booking).booking_confirm.deliver_now
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_07_095906) do
+ActiveRecord::Schema.define(version: 2023_02_21_201829) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer "user_id"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2020_09_07_095906) do
     t.string "state"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "aasm_state"
     t.index ["host_id"], name: "index_bookings_on_host_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
